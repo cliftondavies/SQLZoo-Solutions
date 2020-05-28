c891b6ba4de8d3b9b4ff415671325c20c18ea388
Clifton Davies <clifton.davies@outlook.com>
2020-05-28 01:44:00 +0100

    Add quiz answers for Select within Select

--- select_within_select.sql
+++ select_within_select.sql
@@ -67,4 +67,51 @@ FROM world x
 WHERE name <= ALL
 (SELECT name
 FROM world y
-WHERE y.continent = x.continent AND name IS NOT NULL);               
+WHERE y.continent = x.continent AND name IS NOT NULL);    
+               
+
+-- QUIZ
+               
+-- 1. Select name, region and population of the smallest country in each region
+SELECT region, name, population
+FROM bbc x
+WHERE population <= ALL
+(SELECT population
+FROM bbc y
+WHERE y.region = x.region AND population > 0);
+               
+-- 2. Select countries belonging to regions with all populations over 50000
+SELECT name, region, population
+FROM bbc x
+WHERE 50000 < ALL
+(SELECT population
+FROM bbc y
+WHERE x.region = y.region AND y.population > 0); 
+               
+-- 3. Select countries with a less than a third of the population of the countries around it
+SELECT name, region
+FROM bbc x
+WHERE population < ALL
+(SELECT population / 3
+FROM bbc y
+WHERE y.region = x.region AND y.name != x.name);
+               
+-- 5. Select countries with a greater GDP than any country in Africa (some countries may have NULL gdp values).
+SELECT name
+FROM bbc
+WHERE gdp >
+(SELECT MAX(gdp)
+FROM bbc
+WHERE region = 'Africa');
+
+-- 6. Select countries with population smaller than Russia but bigger than Denmark
+SELECT name
+FROM bbc
+WHERE population <
+(SELECT population
+FROM bbc
+WHERE name = 'Russia')
+AND population >
+(SELECT population
+FROM bbc
+WHERE name = 'Denmark');             
