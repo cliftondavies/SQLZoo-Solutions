aed32e3e31bf67b4605cabb0a163b3e5929fc41b
Clifton Davies <clifton.davies@outlook.com>
2020-05-28 01:50:04 +0100

    Update quiz answers on select_basics.sql

--- select_basics.sql
+++ select_basics.sql
@@ -18,22 +18,22 @@ WHERE area BETWEEN 200000 AND 250000;
 
 -- QUIZ
 
--- Q1
+-- 1. Select countries with population between 1000000 and 1250000
 SELECT name, population
 FROM world
 WHERE population BETWEEN 1000000 AND 1250000;
 
--- Q3
+-- 3. Select countries that end in A or L
 SELECT name
 FROM world
 WHERE name LIKE '%a' OR name LIKE '%l';
 
--- Q6
+-- 6. Select countries with an area larger than 50000 and a population smaller than 10000000
 SELECT name, area, population
 FROM world
 WHERE area > 50000 AND population < 10000000;
 
--- Q7
+-- 7.  Select the population density of China, Australia, Nigeria and France
 SELECT name, population/area
 FROM world
 WHERE name IN ('China', 'Nigeria', 'France', 'Australia');
