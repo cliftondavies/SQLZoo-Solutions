82aaffb36dda3ae9cf80c10d3722da472e387804
Clifton Davies <clifton.davies@outlook.com>
2020-05-29 18:48:14 +0100

    Indent nested Select statements

--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -79,30 +79,37 @@ END, subject, winner;
 -- QUIZ
 
 -- 1. Select winner's names beginning with C and ending in n
-SELECT winner FROM nobel
+SELECT winner
+FROM nobel
 WHERE winner LIKE 'C%' AND winner LIKE '%n';
  
 -- 2. Select how many Chemistry awards were given between 1950 and 1960
-SELECT COUNT(subject) FROM nobel
-WHERE subject = 'Chemistry'
-AND yr BETWEEN 1950 and 1960;
+SELECT COUNT(subject)
+FROM nobel
+WHERE subject = 'Chemistry' AND yr BETWEEN 1950 and 1960;
 
 -- 3. Select the amount of years where no Medicine awards were given
-SELECT COUNT(DISTINCT yr) FROM nobel
-WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine');
+SELECT COUNT(DISTINCT yr)
+FROM nobel
+WHERE yr NOT IN
+ (SELECT DISTINCT yr
+ FROM nobel
+ WHERE subject = 'Medicine');
 
 -- 5. Select the year when neither a Physics or Chemistry award was given
-SELECT yr FROM nobel
-WHERE yr NOT IN
-(SELECT yr
+SELECT yr
 FROM nobel
-WHERE subject IN ('Chemistry', 'Physics'));
+WHERE yr NOT IN
+ (SELECT yr
+ FROM nobel
+ WHERE subject IN ('Chemistry', 'Physics'));
 
 -- 6. Select the years when a Medicine award was given but no Peace or Literature award was
 SELECT DISTINCT yr
 FROM nobel
 WHERE subject = 'Medicine' AND yr NOT IN
-(SELECT yr FROM nobel 
-WHERE subject = 'Literature')
-AND yr NOT IN (SELECT yr FROM nobel
-WHERE subject = 'Peace');
+ (SELECT yr FROM nobel 
+ WHERE subject = 'Literature')
+AND yr NOT IN
+ (SELECT yr FROM nobel
+ WHERE subject = 'Peace');
