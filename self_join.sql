941e0046667d91ffb9b71ccd07bad8c9719f4ed2
Clifton Davies <clifton.davies@outlook.com>
2020-05-30 02:46:41 +0100

    Add quiz answers to self_join.sql

--- self_join.sql
+++ self_join.sql
@@ -68,3 +68,35 @@ ON a.company = b.company AND a.num = b.num
 INNER JOIN stops
 ON b.stop = stops.id
 WHERE a.stop = 53 AND a.company = 'LRT';
+
+-- 10. Yet to be solved
+
+
+-- QUIZ
+
+-- 1. Select the code that would show it is possible to get from Craiglockhart to Haymarket
+SELECT DISTINCT a.name, b.name
+FROM stops a
+INNER JOIN route z
+ON a.id = z.stop
+INNER JOIN route y
+ON y.num = z.num
+INNER JOIN stops b
+ON y.stop = b.id
+WHERE a.name = 'Craiglockhart' AND b.name = 'Haymarket';
+
+-- 2. Stops that are on route.num '2A' which can be reached with one bus from Haymarket
+SELECT S2.id, S2.name, R2.company, R2.num
+FROM stops S1, stops S2, route R1, route R2
+WHERE S1.name = 'Haymarket' AND S1.id = R1.stop AND R1.company = R2.company AND R1.num = R2.num AND R2.stop=S2.id AND R2.num='2A';
+
+-- 3. Services available from Tollcross
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a
+INNER JOIN route b
+ON a.company = b.company AND a.num=b.num
+INNER JOIN stops stopa
+ON a.stop = stopa.id
+INNER JOIN stops stopb
+ON b.stop = stopb.id
+WHERE stopa.name = 'Tollcross';
